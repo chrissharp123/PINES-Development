caefb0445e9e39c0d7d8e3362a36b6243399f975
Chris Sharp <csharp@georgialibraries.org>
2011-04-20 09:14:13 -0400

    adding PINES survey page and utility scripts

---
+++ PINES-files/home/opensrf/survey-down.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /openils/var/web/opac/skin/default/xml/home/
+rm homesearch.xml
+mv homesearch.xml.orig homesearch.xml
+
---
+++ PINES-files/home/opensrf/survey-up.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /openils/var/web/opac/skin/default/xml/home/
+mv homesearch.xml homesearch.xml.orig
+cp homesearch.xml.survey homesearch.xml
