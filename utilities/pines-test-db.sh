967f860305bc44f39cee43dcef9c70da86a21508
Chris Sharp <csharp@georgialibraries.org>
2011-07-13 06:44:08 -0400

    adding GPL

--- utilities/pines-test-db.sh
+++ utilities/pines-test-db.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 # 
 # Chris Sharp <csharp@georgialibraries.org> 
-# (c)2011 Georgia Public Library Service
+# Copyright 2011 Georgia Public Library Service
 # 
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
 # A script to install the PINES database on a test instance.  It presumes that
 # your site follows the Continuous Backup and PITR procedures detailed in the 
 # PostreSQL documentation: 
